4341a2b66f4308a090fa3bc698052ba2ee726ec8
deviant <mb.deviant@gmail.com>
2022-09-18 20:54:47 +0300

    create grid by default on page load

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const container = document.querySelector('.grid-container');
-
 function formGrid() {
-    for (let i = 1; i <= 16; i++) {
+    for (let i = 1; i <= 256; i++) {
         const div = document.createElement('div');
         div.classList.add("grid-item");
         div.textContent = `${i}`
         container.appendChild(div);
     }
 }
+window.onload = formGrid();
