61514e940ac6bc8cfb9c7c8f9694ee5efc9d4ba8
deviant <mb.deviant@gmail.com>
2022-09-20 23:12:38 +0300

    update grid function

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const container = document.getElementById("container");
 
 function formGrid(row, col) {
-     container.style.setProperty('--grid-row', row);
-     container.style.setProperty('--grid-col', col);
+    container.style.setProperty('--grid-row', row);
+    container.style.setProperty('--grid-col', col);
     for (let i = 0; i < (row * col); i++) {
         let gridCell = document.createElement('div');
         container.appendChild(gridCell).className = 'grid-item';
     };
 };
 
-window.onload = formGrid(100,100);
+window.onload = formGrid(100, 100);
 
 
