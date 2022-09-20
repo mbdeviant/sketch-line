bde9444a9ab7dab08b506ae672a3250600c639ca
deviant <mb.deviant@gmail.com>
2022-09-21 01:21:22 +0300

    add color change effect on mouseover

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const container = document.getElementById("container");
 
+
 function formGrid(row, col) {
     container.style.setProperty('--grid-row', row);
     container.style.setProperty('--grid-col', col);
     for (let i = 0; i < (row * col); i++) {
         let gridCell = document.createElement('div');
         container.appendChild(gridCell).className = 'grid-item';
+        gridCell.addEventListener('mouseover',()=>{
+            gridCell.style.background = "black";
+        })
     };
+
 };
-
-window.onload = formGrid(100, 100);
-
-
+window.onload = formGrid(75,75);
