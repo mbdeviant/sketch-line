6df770e824fb2b685e8f10e93441924e88287a27
deviant <mb.deviant@gmail.com>
2022-09-21 17:22:43 +0300

    add & edit grid resizer button

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const container = document.getElementById("container");
 
-
 function formGrid(row, col) {
     container.style.setProperty('--grid-row', row);
     container.style.setProperty('--grid-col', col);
     for (let i = 0; i < (row * col); i++) {
         let gridCell = document.createElement('div');
         container.appendChild(gridCell).className = 'grid-item';
-        gridCell.addEventListener('mouseover',()=>{
+        gridCell.addEventListener('mouseover', () => {
             gridCell.style.background = "black";
-        })
+        });
     };
 
 };
-window.onload = formGrid(75,75);
+window.onload = formGrid(75, 75);
