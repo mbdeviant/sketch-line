f9229c9573d28f645629cd259ef78a01590d21c2
deviant <mb.deviant@gmail.com>
2022-09-23 17:06:08 +0300

    add function to rgb button

--- script.js
+++ script.js
@@ -9,34 +9,41 @@ window.onload = formGrid(16);
 
 function formGrid(gridSize) {
     let gridArea = gridSize * gridSize;
-    for (let i = 1; i <= gridArea ; i++) {
+    for (let i = 1; i <= gridArea; i++) {
         let gridCell = document.createElement('div');
         container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
         container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
         container.appendChild(gridCell);
     };
     var gridCells = container.querySelectorAll('div');
-    gridCells.forEach(gridCells => gridCells.addEventListener('mouseover', ()=> gridCells.style.background ="black") );
-
+    gridCells.forEach(gridCells => gridCells.addEventListener('mouseover', () => gridCells.style.background = "black"));
 };
+rgbBtn.addEventListener('click', () => {
+    rgbBtn.classList.add("clicked");
+    var gridCells = container.querySelectorAll('div');
+    gridCells.forEach(gridCells => gridCells.addEventListener('mouseover', () => {
+        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        gridCells.style.background = "#" + randomColor;
+    }));
+});
 
 resizeBtn.addEventListener('click', () => {
     let promptGridSize = prompt("Maximum 100, defines rows and columns");
     let gridSize = parseInt(promptGridSize);
     if (promptGridSize == null) return;
-    else if (isNaN(gridSize)){
+    else if (isNaN(gridSize)) {
         window.alert("a number between 1 and 100 please");
         return;
     }
-    else if (gridSize < 0 || gridSize > 100 ) {
+    else if (gridSize < 0 || gridSize > 100) {
         window.alert("a number between 1 and 100 please");
         return;
     }
     var gridCells = container.querySelectorAll('div');
-    gridCells.forEach(gridCells  => gridCells.style.background = "white");
+    gridCells.forEach(gridCells => gridCells.style.background = "white");
     formGrid(gridSize);
 });
-clearBtn.addEventListener('click',()=> {
+clearBtn.addEventListener('click', () => {
     var gridCells = container.querySelectorAll('div');
     gridCells.forEach(gridCells => gridCells.style.background = "white");
 });
